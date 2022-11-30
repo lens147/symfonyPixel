4c2307fac3197e78583fcfad14dad7c84b9a2c68
lens147 <bartunior@gmail.com>
2022-11-30 15:58:18 +0100

    formLogin:1.0.1

--- src/Security/LoginFormeAuthenticator.php
+++ src/Security/LoginFormeAuthenticator.php
@@ -47,8 +47,9 @@ class LoginFormeAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+
+        /* throw new \Exception('TODO: provide a valid redirect inside '.__FILE__); */
+        return new RedirectResponse($this->urlGenerator->generate('some_route'));
     }
 
     protected function getLoginUrl(Request $request): string
